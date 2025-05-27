077fbdcd9f37d097f890cd1380affff35218eec6
D1nho0 <craudiobernardo13@gmail.com>
2025-05-27 16:58:01 -0300

    up

---
+++ Cursos/cursos.js
@@ -0,0 +1,16 @@
+        function toggleDropdown() {
+            document.getElementById("dropdownMenu").classList.toggle("show");
+        }
+        
+        // Fechar o dropdown se clicar fora dele
+        window.onclick = function(event) {
+            if (!event.target.matches('.perfil')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
